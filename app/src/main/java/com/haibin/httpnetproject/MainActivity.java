2fa7162eef86b1f1866c9e32cc023412d73cec79
haibin <huanghaibin_dev@163.com>
2016-09-13 09:29:08 +0800

    init project

--- app/src/main/java/com/haibin/httpnetproject/MainActivity.java
+++ app/src/main/java/com/haibin/httpnetproject/MainActivity.java
@@ -3,11 +3,45 @@ package com.haibin.httpnetproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.haibin.httpnet.HttpNetClient;
+import com.haibin.httpnet.builder.Request;
+import com.haibin.httpnet.builder.RequestParams;
+import com.haibin.httpnet.core.Response;
+import com.haibin.httpnet.core.call.CallBack;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HttpNetClient client = new HttpNetClient();
+        Request request = new Request.Builder()
+                .encode("UTF-8")
+                .method("GET")
+                .timeout(13000)
+                .url("http://www.oschina.net")
+                .build();
+        client.newCall(request).execute(new CallBack() {
+            @Override
+            public void onResponse(Response response) {
+
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+        });
+        RequestParams params = new RequestParams()
+                .put("userName","oscer")
+                .put("pwd","oschina");
+        Request request1 = new Request.Builder()
+                .encode("UTF-8")
+                .method("POST")
+                .params(params)
+                .timeout(13000)
+                .url("http://www.oschina.net")
+                .build();
     }
 }
